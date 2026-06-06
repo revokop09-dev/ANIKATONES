c4633cbbf3bca389e32863848f16d6f3565ddac2
Gw <revokop09@gmail.com>
2026-06-06 20:15:54 +0530

    Fix typo in logo text

--- components/page/logo.jsx
+++ components/page/logo.jsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <Link href="/" className="select-none">
             <div>
-                <h1 className="text-2xl font-bold">𝚲𝛈𝛊፝֟ᴋ𝛂 <span className="opacity-50">𝐌ᴜsɪᴄ</span></h1>
+                <h1 className="text-2xl font-bold">𝚲𝛈𝛊ᴋ𝛂 <span className="opacity-50">𝐌ᴜsɪᴄ</span></h1>
             </div>
         </Link>
     )
